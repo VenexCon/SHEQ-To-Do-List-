88bf1741c308c647d9cb4031136dec83a7e682f1
VenexCon <venexconsultant@outlook.co.uk>
2022-07-18 23:06:31 +0100

    <update> updated factory.js with formGrab and collectFormData

--- src/app/factory.js
+++ src/app/factory.js
@@ -1,7 +1,7 @@
 
 //factory Function 
 
-function taskobject (title, date, priority, catagorey, description) {
+function Taskobject (title, date, priority, catagorey, description) {
   return {  
     title,
     date,
@@ -9,4 +9,33 @@ function taskobject (title, date, priority, catagorey, description) {
     catagorey,
     description,
   }
-}
+};
+
+function collectFormData (e) {
+
+  e.preventDefault();
+  console.log("form prevented")
+    //collect values
+
+    const title = document.getElementById("task-title").value;
+    const date = document.getElementById("task-date").value;
+    const priority = document.getElementById("task-priority").value;
+    const catagorey = document.getElementById("task-catagorey").value;
+    const description = document.getElementById("task-description").value;
+
+    let newTask = Taskobject(title, date, priority,catagorey,description)
+    return console.log(newTask);
+
+};
+
+
+function collectForm (e) {
+  const form = document.getElementById("task-form");
+      form.addEventListener("submit", function (e) {
+        collectFormData(e);
+      });
+};
+
+
+
+export {collectForm}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import "./style/style.scss";
 import "./imgs/todoIcon.png"
 import { DOMGrabs } from "./app/dom";
+import { collectForm } from "./app/factory";
 
-const DOM = function domEvents () {
-    DOMGrabs()
+
+
+const DOM = function domEvents (e) {
+    DOMGrabs(e)
+    collectForm(e)
 }
+
+
 
 window.onload = DOM();
 
