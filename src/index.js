5452db12b0ef40c2088adb65e6881d77f448fd40
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-07-27 10:59:08 +0100

    Update index.js with import from storage.js
    Imported localStored Object from storage.js, storage functions will now be placed here and exported, with the object imported into each module.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "./style/style.scss";
 import { DOMGrabs } from "./app/dom";
 import { collectForm, createFromStorage} from "./app/factory";
+import {localStored} from "./storage.js";
 
 
 
 const DOM = function domEvents (e) {
     DOMGrabs(e);
     collectForm(e);
-    createFromStorage();
+    localStored.create()
    
 };
 
