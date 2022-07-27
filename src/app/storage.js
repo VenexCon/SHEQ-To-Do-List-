6a0076b1cee7b03684bc6a03e2aa28c2e660ce74
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-07-27 11:54:10 +0100

    Update storage.js

--- src/app/storage.js
+++ src/app/storage.js
@@ -1,7 +1,7 @@
 import {createNewHero} from "./dom.js"
 
 
-const storedObjects = () => {
+function storedObjects () {
         let storedobjects = [];
         for(const key in window.localStorage){
          let object = JSON.parse(window.localStorage.getItem(key))
@@ -11,14 +11,14 @@ const storedObjects = () => {
           return storedobjects
         };
 
-const createCardFromLS = () => {
+function createCardFromLS (){
             let storedobjects = storedObjects();
               storedobjects.forEach(object => {
                 createNewHero(object);
             })
         };
 
-const sendToLocalStorage = (object) => {
+function sendToLocalStorage (object) {
             const {title} = object;
             return window.localStorage.setItem(`${title}`, JSON.stringify(obj));
     };
